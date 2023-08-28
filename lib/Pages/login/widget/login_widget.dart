0ebe3cca573bfa015c33b061e6a744df26007c96
souksanDev1 <souksanchanthaphone@gmail.com>
2023-08-28 16:24:07 +0700

    g

--- lib/Pages/login/widget/login_widget.dart
+++ lib/Pages/login/widget/login_widget.dart
@@ -26,7 +26,7 @@ AppBar buildAppBar() {
 // ignore: non_constant_identifier_names
 Widget BuildThirdPartyLogin(BuildContext context) {
   return Column(
-    children: [BuildIcon(), buildLogin(context), Text("8888"), Text("99999")],
+    children: [BuildIcon(), buildLogin(context), Text("8888"), Text("yyyyyy")],
   );
 }
 
